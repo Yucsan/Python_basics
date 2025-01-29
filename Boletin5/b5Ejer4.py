e55bf51528e8f03748b29e49c8f2e2780d09881a
Yucsan <yucsan2023@gmail.com>
2025-01-29 18:01:11 +0100

    safe1

--- Boletin5/b5Ejer4.py
+++ Boletin5/b5Ejer4.py
@@ -4,6 +4,7 @@ determinado hospital perteneciente a una aseguradora privada.
 Sabemos que a través del dni del paciente queremos tener acceso a: 
 nombre, edad, si ha sido atendido previamente en ese hospital 
 y al código (valor numérico entero) de su médico de cabecera.
+
 - Visualiza la información almacenada.
 - Calcula la edad media de todos los pacientes que han sido atendidos alguna vez en ese hospital.
 - Visualiza el listado de todos los pacientes de un determinado médico cuyo código se solicitará por teclado.
@@ -11,28 +12,27 @@ y al código (valor numérico entero) de su médico de cabecera.
 
 """
 
-estudiantes = {}
-alumno = {}
+#vamos a Probar
+from random import *
 
-for i in range(2):
-    nombre = input('inserta un nombre: ')
-    nota = input('inserta nota: ')
-    alumno['nombre'] = nombre
-    alumno['nota'] = int(nota)
-    estudiantes[i] = alumno
-    alumno = {}
+def generaId():
+    return random.randint(10000000, 99999999)
 
-print("APROBADOS")
-for i in estudiantes:
-    if estudiantes[i]['nota'] > 4:
-        print(estudiantes[i])
+def edad():
+    return random.randint(15,89)
 
-print("DESAPROBADOS")
-for i in estudiantes:
-    if estudiantes[i]['nota'] < 5:
-        print(estudiantes[i])
+def aten():
+    return random.randint(1,2)
 
-#print(estudiantes)
+
+
+paciente = {
+    "idmed": 123,
+    "dni": "12345678Z",
+    "nombre": "Feliz",
+    "edad": 35,
+    "aten": 1
+}
 
 print("fin")
 
