8f39d6f15791d1c8344e0b74367e99f66d425800
Yucsan <yucsan2023@gmail.com>
2025-02-04 13:46:27 +0100

    inicio boletin ficheros

---
+++ test.py
@@ -0,0 +1,12 @@
+list(enumerate(["Tomate", "Cebolla", "Pimiento", "Pepino", "Ajo", "Aceite", "Vinagre"]))
+
+
+#Para mostrar los valores podríamos hacer algo así: 
+
+huerto=list(enumerate(["Tomate", "Cebolla", "Pimiento", "Pepino", "Ajo", "Aceite", "Vinagre"]))
+
+huerto2=list(enumerate({"Tomate", "Cebolla", "Pimiento", "Pepino", "Ajo", "Aceite", "Vinagre"}))
+
+print(huerto)
+
+print(huerto2)
